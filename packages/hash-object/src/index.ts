c1ebcd998dba275147066afbc608211f1430d329
V8 macOS Builder <v8.macos.builder@localhost>
2023-05-27 20:38:48 +0100

    fixed hash-object

--- packages/hash-object/src/index.ts
+++ packages/hash-object/src/index.ts
@@ -1,13 +1,15 @@
+import CRC32 from "crc-32";
+
 export const hash = (object: any) => {
   const seen = new Set<any>();
   const json = JSON.stringify(object, function (key, value) {
-    seen.add(this);
-    if (typeof value === 'function') {
+    if (typeof value === "function") {
       return value.displayName ?? value.name;
     } else if (seen.has(this)) {
-      return '#CircularReference';
+      return "#CircularReference";
     }
+    seen.add(this);
     return value;
   });
-  return json;
+  return CRC32.str(json).toString(16);
 };
